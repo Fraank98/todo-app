a760f280d9b9c28e06fdbd376006909b7518c564
Fraank98 <danilo806@icloud.com>
2023-05-14 23:21:10 +0200

    add sliding gesture

--- components/TodoCard.tsx
+++ components/TodoCard.tsx
@@ -9,14 +9,8 @@ export default function TodoCard({ content }: Todo) {
   const tw = useTailwind();
 
   return (
-    <TouchableOpacity onPress={() => console.log("item pressed")}>
-      <View
-        style={tw(
-          "w-full rounded-lg p-3 my-2 border-solid border bg-[#0085ff]"
-        )}
-      >
-        <Text style={tw("text-white uppercase font-bold")}>{content}</Text>
-      </View>
-    </TouchableOpacity>
+    <View style={tw("w-full rounded-lg p-3 my-2 bg-[#0085ff]")}>
+      <Text style={tw("text-white uppercase font-bold")}>{content}</Text>
+    </View>
   );
 }
--- components/TodoView.tsx
+++ components/TodoView.tsx
@@ -1,11 +1,17 @@
-import { View, FlatList } from "react-native";
+import { View, FlatList, Text } from "react-native";
 import TodoCard from "./TodoCard";
 import { useTailwind } from "tailwind-rn";
+import { SwipeListView } from "react-native-swipe-list-view";
+
+type Todo = {
+  id: number;
+  content: string;
+};
 
 export default function TodoView() {
   const tw = useTailwind();
 
-  const todos = [
+  const todos: Todo[] = [
     { id: 1, content: "pranzare" },
     { id: 2, content: "bere" },
     { id: 3, content: "giocare" },
@@ -26,11 +32,26 @@ export default function TodoView() {
     { id: 18, content: "pranzare" },
   ];
 
+  function renderDeleteButton() {
+    return (
+      <View
+        style={tw(
+          "bg-red-600 rounded-lg flex items-center justify-center absolute p-3 my-2 right-0 w-[20%]"
+        )}
+      >
+        <Text style={tw("text-white")}>Delete</Text>
+      </View>
+    );
+  }
+
   return (
     <View style={tw("w-full h-[80%] flex px-5 pt-2")}>
-      <FlatList
+      <SwipeListView
         data={todos}
         renderItem={({ item }) => <TodoCard content={item.content} />}
+        disableRightSwipe
+        renderHiddenItem={renderDeleteButton}
+        rightOpenValue={-75}
         keyExtractor={(todo) => todo.id.toString()}
         showsVerticalScrollIndicator={false}
       />
